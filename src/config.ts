d62d1b6a87e59538a074740972b9ab9cbc3b9afb
Grellim <gre.jagua@gmail.com>
2025-07-16 22:45:00 -0300

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -12,12 +12,12 @@ export const siteConfig: SiteConfig = {
 	subtitle: "O conhecer liberta.",
 	lang: "pt", // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
 	themeColor: {
-		hue: 262, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+		hue: 45, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
 		fixed: true, // Hide the theme color picker for visitors
 	},
 	banner: {
-		enable: false,
-		src: "assets/images/demo-banner.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+		enable: true,
+		src: "https://i.imgur.com/CaHb5Mq.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
 		credit: {
 			enable: false, // Display the credit text of the banner image
@@ -53,7 +53,7 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "https://imgur.com/b66054be-6762-4b83-9d35-46e20edc087d", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	avatar: "https://i.imgur.com/XLwIxUw.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "Camarada Greg",
 	bio: "Brasileiro, patriota. Apoiador da causa operária. Acredito na revolução e ditadura do proletáriado, sendo o poder do povo soberano. Tentando explicar de formá mais fácil, o que aprendi do jeito difícil!",
 	links: [
