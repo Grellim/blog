66206b2e57b6a35edc1d358fb985dddbd1f86838
Grellim <gre.jagua@gmail.com>
2025-07-16 22:54:15 -0300

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -67,7 +67,7 @@ export const profileConfig: ProfileConfig = {
 		{
 			name: "Saccey",
 			icon: "fa6-brands:pix",
-			url: "https://saccey.netlify.com",
+			url: "https://saccey.netlify.app",
 		},
 		{
 			name: "Instagram",
@@ -86,5 +86,5 @@ export const licenseConfig: LicenseConfig = {
 export const expressiveCodeConfig: ExpressiveCodeConfig = {
 	// Note: Some styles (such as background color) are being overridden, see the astro.config.mjs file.
 	// Please select a dark theme, as this blog theme currently only supports dark background color
-	theme: "github-dark",
+	theme: "dark",
 };
