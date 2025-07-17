1c19902ccf4e63894f8ef69e7b0ddcce28172286
Grellim <gre.jagua@gmail.com>
2025-07-16 21:58:46 -0300

    Update and rename es.ts to pt.ts

---
+++ src/i18n/languages/pt.ts
@@ -0,0 +1,38 @@
+import Key from "../i18nKey";
+import type { Translation } from "../translation";
+
+export const es: Translation = {
+	[Key.home]: "Inicio",
+	[Key.about]: "Sobre Mim",
+	[Key.archive]: "Arquivo",
+	[Key.search]: "Buscar",
+
+	[Key.tags]: "Tags",
+	[Key.categories]: "Categorias",
+	[Key.recentPosts]: "Mais recentes",
+
+	[Key.comments]: "Comentários",
+
+	[Key.untitled]: "Sem titulo",
+	[Key.uncategorized]: "Sem categoria",
+	[Key.noTags]: "Sem tags",
+
+	[Key.wordCount]: "palavra",
+	[Key.wordsCount]: "palavras",
+	[Key.minuteCount]: "minuto",
+	[Key.minutesCount]: "minutos",
+	[Key.postCount]: "publicação",
+	[Key.postsCount]: "publicações",
+
+	[Key.themeColor]: "Color do tema",
+
+	[Key.lightMode]: "Claro",
+	[Key.darkMode]: "Escuro",
+	[Key.systemMode]: "Sistema",
+
+	[Key.more]: "Mais",
+
+	[Key.author]: "Autor",
+	[Key.publishedAt]: "Publicado em",
+	[Key.license]: "Licensa",
+};
